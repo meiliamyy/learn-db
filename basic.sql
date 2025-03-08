2924144ca8b4eb2945bb1662f2ed1fac52365880
meiliamyy <meiliarahmi8@gmail.com>
2025-03-08 22:39:02 +0700

    add modified basic.sql

---
+++ basic.sql
@@ -0,0 +1,23 @@
+resume dasar query 
+1. select 
+mengambil semua data 
+SELECT * FROM Album;
+
+2. WHERE
+filter data spesifik
+SELECT * FROM Artist WHERE Name = 'The Postal Service';
+
+3. LIMIT 
+membatasi  jumlah data yang ingin kita ambil 
+
+4. ORDER BY 
+mengurutkan data 
+SELECT * FROM Artist ORDER BY Name ASC LIMIT 5;
+
+5. INSERT 
+menambah data baru 
+ INSERT INTO Artist (Name) VALUES ('Radiohead');
+
+
+6. UPDATE
+mengubah data yang sudah ada 
